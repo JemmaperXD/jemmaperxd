79526611c4ecbd2b95b49894eb14a969ee39ae35
JemmaperXD <jemmaperuwu2023@gmail.com>
2026-01-13 02:14:49 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,8 +4,18 @@ modem.open(PORT)
 
 local users = {} -- [id] = name
 
+-- Функция шифрования (Ключ 7, чтобы клиент смог расшифровать)
+local function encrypt(text, key)
+    local res = ""
+    for i = 1, #text do
+        res = res .. string.char((text:byte(i) + key) % 256)
+    end
+    return res
+end
+
 print("=== SERVER STARTED ===")
 print("My ID: " .. os.getComputerID())
+print("Waiting for clients...")
 
 while true do
     local _, _, channel, replyID, msg = os.pullEvent("modem_message")
@@ -33,12 +43,17 @@ while true do
             end
 
             if targetID then
+                -- ШИФРУЕМ сообщение перед пересылкой (Ключ 7)
+                local encrypted_msg = encrypt(msg.text, 7)
+                
                 modem.transmit(targetID, PORT, {
                     type = "msg",
                     from = msg.user,
-                    text = msg.text -- Шифрование добавим когда заработает связь
+                    text = encrypted_msg
                 })
-                print("Msg: " .. msg.user .. " -> " .. msg.to)
+                print("Msg: " .. msg.user .. " -> " .. msg.to .. " (Encrypted)")
+            else
+                print("Error: Target " .. tostring(msg.to) .. " not found!")
             end
         end
     end
